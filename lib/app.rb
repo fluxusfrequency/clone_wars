24e316636e58b429e9bfff4bf2003a88cd33e62e
Ben Lewis <bennlewis@gmail.com>
2013-10-29 10:21:55 -0600

    changes to view files

--- lib/app.rb
+++ lib/app.rb
@@ -5,35 +5,16 @@ require 'sinatra/base'
 class CloneWarApp < Sinatra::Base
 
   register Sinatra::About
+  register Sinatra::BikeShop
+  register Sinatra::Events
+  register Sinatra::Links
+  register Sinatra::Program
+  register Sinatra::Support
 
   get '/' do
     slim :home
   end
 
-  get '/index/about' do
-    slim :about
-  end
-
-  get '/index/programs' do
-
-  end
-
-  get '/index/bike-shop' do
-
-  end
-
-  get '/index/support' do
-
-  end
-
-  get '/index/events' do
-
-  end
-
-  get '/index/links' do 
-
-  end
-  
 #--- googlemaps link
   
   get 'https://maps.google.com/maps?ie=UTF8&cid=10676092889539153742&q=The+Bike+Depot&iwloc=A&gl=US&hl=en-US' do
--- lib/app/views/renamer.rb
+++ lib/app/views/renamer.rb
@@ -1,11 +1,14 @@
 require 'fileutils'
 
-Dir["./*.html.slim"].each do |file|
-  filename = file.to_s.gsub(/(\.\/)*(\.html)*/, "")
+Dir["./*.slim"].each do |file|
+  filename = file.to_s.gsub(/\s+/, "")
+  filename = filename.to_s.gsub(/\&/, "")
+  filename = filename.to_s.gsub(/\,/, "")
   puts filename
   this_file = File.open(file, "r").read
   FileUtils.touch("./#{filename}")
   File.open("./#{filename}", "w") do |file|
     file.write(this_file)
   end
+  FileUtils.rm(file)
 end
