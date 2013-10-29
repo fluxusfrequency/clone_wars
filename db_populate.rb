07c6da8de46f368ed75866d19ae1b3aee4e08fe1
Ben Lewis <bennlewis@gmail.com>
2013-10-29 16:11:10 -0600

    populated database

--- db_populate.rb
+++ db_populate.rb
@@ -1,17 +1,61 @@
-require 'nokogiri'
 require 'fileutils'
 require 'sequel'
 
-pages = []
+titles = []
+pgs = []
+urls = [  
+'/about',
+'/programs/bike-camp', 
+'/programs/bike-rodeo', 
+'/bike-shop', 
+'/about/staff-board/17-about/staff-board/43-bill-davis', 
+'/about/staff-board/17-about/staff-board/7-board', 
+'/links/business-partners', 
+'/events/calendar', 
+'/programs/classes', 
+'/about/contact', 
+'/support/donate-bikes', 
+'/programs/earn-a-bike', 
+'/events', 
+'/programs/fix-your-bike', 
+'/links/friends', 
+'/about/history', 
+'/',
+'/about/staff-board/17-about/staff-board/38-james-pullen', 
+'',
+'/links', 
+'/support/memberships', 
+'/about/staff-board/17-about/staff-board/42-mike-decker', 
+'/about/mission-vision-and-values', 
+'/about/staff-board/17-about/staff-board/44-morgan-matter', 
+'/support/volunteer/21-new-volunteers', 
+'/support/organizing-a-bike-collection-drive', 
+'/events/park-hill-family-bike-ride', 
+'/about/privacy-policy', 
+'/programs', 
+'/bike-shop/retail', 
+'/about/staff-board/17-about/staff-board/39-ryne-emory', 
+'/bike-shop/service', 
+'/about/staff-board', 
+'/support', 
+'/support/volunteer', 
+'/links/we-love', 
+'/support/wish-list', 
+'/bike-shop/xtracycle', ]
+
+database = Sequel.sqlite('clone_wars.sqlite3')
+pages = database.from(:pages).to_a
 
 Dir["./lib/app/views/*.erb"].each do |file|
-  pages << File.open(file, "r")
+  filename = file.to_s.gsub(/.erb/, "")
+  filename = filename.gsub(/\.\/lib\/app\/views\//, "")
+  titles << filename
+  pgs << File.open(file, "r").read
 end
 
-pages.each do |page|
-  loaded = Nokogiri::HTML(page)
-  body = loaded.css("div#content-w2")
-  title = loaded.title.to_s
-  url = loaded.document.url
+
+pgs.each_with_index do |page, i|
+  hash = {:title => titles[i], :body => pgs[i], :url => urls[i]}
+  database.from(:pages).insert(hash)
 end
 
