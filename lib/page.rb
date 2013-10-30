230b474b2772a01b30e60f22a97e4d15ac5726bc
Ben Lewis <bennlewis@gmail.com>
2013-10-30 10:20:38 -0600

    built CRUD functionality on Page Store

--- lib/page.rb
+++ lib/page.rb
@@ -3,10 +3,10 @@ class Page
   attr_accessor :title, :body, :id, :url
 
   def initialize(attributes = {})
-    @id = attributes["id"]
-    @title = attributes["title"]
-    @body = attributes["body"]
-    @url = attributes["url"]
+    @id = attributes["id"] || nil
+    @title = attributes["title"] || "Title"
+    @body = attributes["body"] || "Body"
+    @url = attributes["url"] || "Url"
   end
 
   def to_h
